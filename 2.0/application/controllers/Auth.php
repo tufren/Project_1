cf37a3f45c721c63d04526c54af4a8424b871c7f
Tufren <novalto723@gmail.com>
2021-11-13 22:48:52 +0700

    Menambahkan file baru dan membuat file navigasi

--- 2.0/application/controllers/Auth.php
+++ 2.0/application/controllers/Auth.php
@@ -3,17 +3,89 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
-		$this->load->view('_temp/Head');
-		$this->load->view('auth/login');
-		$this->load->view('_temp/Foot');
+		$this->form_validation->set_rules('Email', 'Email', 'required|trim|valid_email');
+		$this->form_validation->set_rules('Password', 'Password', 'required|trim');
+		
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = 'Login';
+			$this->load->view('_temp/Head', $data);
+			$this->load->view('auth/login');
+			$this->load->view('_temp/Foot');
+		} else {
+			$this->_login();
+		}
+	}
+
+	private function _login()
+	{
+		$alamat = $this->input->post('Email');
+		$pass = $this->input->post('Password');
+
+		$pengguna = $this->db->get_where('user', ['email' => $alamat])->row_array();
+
+		if ($pengguna) {
+			// if pengguna active
+			if ($pengguna['is_active'] = 1) {
+				if (password_verify($pass, $alamat['password'])) {
+					$data = [
+						'email' => $pengguna['email'],
+						'role_id' => $pengguna['role_id']
+					];
+					$this->session->set_userdata($data);
+					redirect('user');
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password!</div>');
+					redirect('auth');
+				}
+			} else {
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has been activated, please check your email!</div>');
+				redirect('auth');
+			}
+		} else {
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This email has not exist, you must register!</div>');
+			redirect('auth');
+		}
 	}
 
 	public function register()
 	{
-		$this->load->view('_temp/Head');
-		$this->load->view('auth/daftar');
-		$this->load->view('_temp/Foot');
+		$this->form_validation->set_rules('Name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('Email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+			'is_unique' => 'This email has been already, choose other email!'
+		]);
+		$this->form_validation->set_rules('Password1', 'Password', 'required|trim|min_length[3]|matches[Password2]', [
+			'matches' => 'Password not matching',
+			'min_length' => 'Password too short'
+		]);
+		$this->form_validation->set_rules('Password2', 'Password', 'required|trim|matches[Password1]');
+		
+		if ($this->form_validation->run() == FALSE) {
+			$data['title'] = 'Registration';
+			$this->load->view('_temp/Head', $data);
+			$this->load->view('auth/daftar');
+			$this->load->view('_temp/Foot');
+		} else {
+			$data = [
+				'name' => htmlspecialchars($this->input->post('Name'), true),
+				'email' => htmlspecialchars($this->input->post('Email'), true),
+				'image' => 'default.jpg',
+				'password' => password_hash($this->input->post('Password1'), PASSWORD_DEFAULT),
+				'role_id' => 2,
+				'is_active' => 1,
+				'date_created' => time()
+			];
+
+			$this->db->insert('user', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congrast, Your email has been ready.<br>You can Login now!</div>');
+			redirect('auth');
+		}
 	}
 }
--- 2.0/application/views/auth/daftar.php
+++ 2.0/application/views/auth/daftar.php
@@ -11,26 +11,26 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create Account</h1>
                             </div>
-                            <form class="user">
+                            <form class="user" method="post" action="<?= base_url('auth/register'); ?>">
                                 <div class="form-group">
                                     <input type="text" class="form-control form-control-user" id="name" name="Name" 
-                                        placeholder="Full Name">
+                                        placeholder="Full Name" value="<?= set_value('Name'); ?>"><?= form_error('Name', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
                                 <div class="form-group">
                                     <input type="text" class="form-control form-control-user" id="email" name="Email" 
-                                        placeholder="Email Address">
+                                        placeholder="Email Address" value="<?= set_value('Email'); ?>"><?= form_error('Email', '<small class="text-danger pl-3">', '</small>'); ?>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
-                                            id="password1" name="Password1" placeholder="Password">
+                                            id="password1" name="Password1" placeholder="Password"><?= form_error('Password1', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
                                     <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user"
                                             id="password2" name="Password2" placeholder="Repeat Password">
                                     </div>
                                 </div>
-                                <button href="login.html" class="btn btn-primary btn-user btn-block">
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                                     Register Account
                                 </button>
                             </form>
